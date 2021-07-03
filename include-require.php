64bcb8d1ce57f69536410db8123d2173687f3f3a
gu1tekin <gultekincirik@gmail.com>
2021-07-03 17:24:43 +0200

    include & require

--- include-require.php
+++ include-require.php
@@ -1,6 +1,6 @@
 <?php
 
-# INCLUDE - REQUIRE
+# INCLUDE & REQUIRE
 /*
     - include, Use include when the file is not required and application should continue when file is not found.
     - require, Use require when the file is required by the application.
