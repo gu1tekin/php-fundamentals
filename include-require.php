6550be8f5ea5c538a3cf196c073ebd6841faeb86
gu1tekin <gultekincirik@gmail.com>
2021-07-03 17:23:04 +0200

    SUPERGLOBALS - $_SERVER

---
+++ include-require.php
@@ -0,0 +1,13 @@
+<?php
+
+# INCLUDE - REQUIRE
+/*
+    - include, Use include when the file is not required and application should continue when file is not found.
+    - require, Use require when the file is required by the application.
+*/
+
+//Include
+include 'header.php';
+
+//Require
+require 'db.php';
